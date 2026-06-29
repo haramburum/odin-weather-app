58b07e97ddd957ef30c269c03ae686cf48ea5ad9
Andrey Sidorkin <gholos@yandex.ru>
2026-06-29 17:36:36 +0700

    feat: add weather icon

--- src/js/screenController.js
+++ src/js/screenController.js
@@ -6,12 +6,13 @@ const screenController = () => {
   const locationElem = document.querySelector(".weather-card__location");
   const tempElem = document.querySelector(".weather-card__temp");
   const descrElem = document.querySelector(".weather-card__description");
+  const cardIcon = document.querySelector(".weather-card__icon");
   const feelsElem = document.querySelector(".details-card__data--feels-like");
   const humidityElem = document.querySelector(".details-card__data--humidity");
   const windElem = document.querySelector(".details-card__data--wind-speed");
   const visibElem = document.querySelector(".details-card__data--visibility");
 
-  const formatTemperature = (temp) =>  {
+  const formatTemperature = (temp) => {
     const roundedTemp = Math.round(temp);
     return temp < 0 ? `-${roundedTemp}` : `+${roundedTemp}`;
   };
@@ -24,6 +25,11 @@ const screenController = () => {
       locationElem.textContent = data.resolvedAddress.split(", ")[0];
       tempElem.textContent = `${formatTemperature(data.currentConditions.temp)}\u00B0C`;
       descrElem.textContent = data.currentConditions.conditions;
+
+      const iconName = data.currentConditions.icon;
+      cardIcon.src = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/58c79610addf3d4d91471abbb95b05e96fb43019/SVG/1st%20Set%20-%20Monochrome/${iconName}.svg`;
+      cardIcon.alt = iconName;
+      console.log(iconName);
 
       feelsElem.textContent = `${formatTemperature(data.currentConditions.feelslike)}\u00B0C`;
       humidityElem.textContent = `${data.currentConditions.humidity}%`;
